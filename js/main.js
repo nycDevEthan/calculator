6467ac334193acc1bef339185c388db2aa0051de
ejolikestocode <ethanoconnell456@gmail.com>
2022-07-06 20:25:58 -0400

    Scrap old code and build again

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 class Calculator {
-    constructor() {
-        this.
+    constructor(number1, number2, operand1, operand2) {
+
     }
 }
 
+let calculator = new Calculator;
+
+let userInput = document.querySelector('button').addEventListener()
+
+console.log(userInput);
