884c316f082528864bebf0400e1dcf6860446ddf
ejolikestocode <ethanoconnell456@gmail.com>
2022-07-10 20:56:30 -0400

    Add JS functionality

--- js/main.js
+++ js/main.js
@@ -8,7 +8,56 @@ class Calculator {
 
 const calculator = new Calculator;
 
-// click number button
+// declare and assign variables
+
+let display = document.getElementById('display');
+const numberButtons = Array.from(document.getElementsByClassName('number'));
+const operatorButtons = Array.from(document.getElementsByClassName('operator'));
+let equalButton = document.getElementById('equals');
+let clearButton = document.getElementById('clear');
+
+let previousValue;
+let currentValue;
+let operator;
+
+// for each element in array, adds 'click' event listener on each button, then for each click event runs function
+
+numberButtons.forEach((element) => element.addEventListener('click', (event) => {
+    if (event.target.innerText === 'Clear') {
+        clearDisplay();
+    } else {
+        display.innerText += event.target.innerText;
+    }
+}));
+
+operatorButtons.forEach((element) => element.addEventListener('click', event => {
+    if (event.target.innerText === '+') {
+        previousValue = display.innerText;
+        clearDisplay();
+        // currentValue += event.target.innerText;
+    } else if (event.target.innerText === '-') {
+        previousValue = display.innerText;
+        clearDisplay();
+    } else if (event.target.class === '/') {
+        previousValue = display.innerText;
+        clearDisplay();
+        currentValue = display.innerText;
+    } else if (event.target.innerText === 'x') {
+        previousValue = display.innerText;
+        clearDisplay();
+    }
+}));
+
+clearButton.addEventListener('click', clearDisplay());
+
+function clearDisplay() {
+    display.innerHTML = null;
+}
+
+// function addNumberToDisplay() {
+//     document.getElementById('display').innerHTML += clickedNumber.innerHTML;
+// }
+
 // display value of number button on screen
 // append numbers clicked to display value on output
 // store in variable
